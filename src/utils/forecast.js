296fb86f43629e39bb397b2f4fb30a54221357a2
nam-web <namsri1998@gmail.com>
2020-12-28 18:28:16 -0800

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback) => {
         }else if(body.error){
             callback('Unable to find the location',undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0] + '. The current temperature is '+ body.current.temperature +'. It feels like '+ body.current.feelslike)
+            callback(undefined,body.current.weather_descriptions[0] + '. The current temperature is '+ body.current.temperature +'. It feels like '+ body.current.feelslike + '. The humidity is '+ body.current.humidity + ' percent.')
         }
     })
 }
